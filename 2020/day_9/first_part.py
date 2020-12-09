b86fc5d2e04e55e2d6716caad6504d0ea8041253
cyrelouyea <alerycserrania@protonmail.com>
2020-12-09 13:42:47 +0100

    Refactor day nine

--- 2020/day_9/first_part.py
+++ 2020/day_9/first_part.py
@@ -1,21 +1,45 @@
 from collections import deque
 from itertools import combinations
-from typing import Deque
+from typing import Deque, Iterable, Optional
+
 
 PREAMBLE_LENGTH = 25
 
-data: Deque[int] = deque()
-for i in range(PREAMBLE_LENGTH):
-    data.append(int(input()))
+
+class XmasCode():
+    _invalid_number: Optional[int]
+
+    def __init__(self, data: Iterable[int]):
+        self._deque = deque(data)
+        self._invalid_number = None
+
+    @property
+    def invalid_number(self) -> int:
+        if self._invalid_number is None:
+            raise Exception("no invalid number")
+        else:
+            return self._invalid_number
+
+    def add(self, number: int) -> int:
+        if self._invalid_number is None and not self._is_number_valid(number):
+            self._invalid_number = number
+        self._deque.append(number)
+        return self._deque.popleft()
+
+    def _is_number_valid(self, number: int) -> bool:
+        for combination in combinations(self._deque, 2):
+            if sum(combination) == new_number:
+                return True
+        return False
 
 
-while True:
+code = XmasCode(
+    (int(input()) for i in range(PREAMBLE_LENGTH))
+)
+
+new_number = int(input())
+while new_number != -1:
+    code.add(new_number)
     new_number = int(input())
-    for combination in combinations(data, 2):
-        if sum(combination) == new_number:
-            data.popleft()
-            data.append(new_number)
-            break
-    else:
-        print("solution:", new_number)
-        break
+
+print("solution:", code.invalid_number)
--- 2020/day_9/second_part.py
+++ 2020/day_9/second_part.py
@@ -1,42 +1,64 @@
 from collections import deque
 from itertools import combinations
-from typing import Deque, List
+from typing import Deque, Iterable, Optional, List
+from typing import overload
+
 
 PREAMBLE_LENGTH = 25
 
-invalid_number = None
-list_numbers: List[int] = list()
-data: Deque[int] = deque()
-for i in range(PREAMBLE_LENGTH):
-    new_number = int(input())
-    data.append(new_number)
-    list_numbers.append(new_number)
 
+class XmasCode():
+    _invalid_number: Optional[int]
+
+    def __init__(self, data: Iterable[int]):
+        self._deque = deque(data)
+        self._list_numbers = list(data)
+        self._invalid_number = None
+
+    @property
+    def invalid_number(self) -> int:
+        if self._invalid_number is None:
+            raise Exception("no invalid number")
+        else:
+            return self._invalid_number
+
+    def add(self, number: int) -> int:
+        if self._invalid_number is None and not self._is_number_valid(number):
+            self._invalid_number = number
+        self._deque.append(number)
+        self._list_numbers.append(number)
+        return self._deque.popleft()
+
+    def find_contiguous_range_set(self) -> range:
+        if self._invalid_number is None:
+            raise Exception("no invalid number")
+
+        for first_index, last_index in combinations(range(len(self._list_numbers)), 2):
+            contiguous_set = self._list_numbers[first_index:last_index+1]
+            if sum(contiguous_set) == self._invalid_number:
+                return range(first_index, last_index)
+
+        raise Exception("no contiguous set found")
+
+    def _is_number_valid(self, number: int) -> bool:
+        for combination in combinations(self._deque, 2):
+            if sum(combination) == new_number:
+                return True
+        return False
+
+    def __getitem__(self, i):
+        return self._list_numbers[i]
+
+
+code = XmasCode(
+    (int(input()) for i in range(PREAMBLE_LENGTH))
+)
+list()
 new_number = int(input())
 while new_number != -1:
-    for combination in combinations(data, 2):
-        if sum(combination) == new_number:
-            data.popleft()
-            data.append(new_number)
-            list_numbers.append(new_number)
-            break
-    else:
-        invalid_number = new_number 
-        list_numbers.append(new_number)
-        break
+    code.add(new_number)
     new_number = int(input())
 
-if new_number == -1:
-    raise Exception("no invalid number found")
-
-while new_number != -1:
-    new_number = int(input())
-    list_numbers.append(new_number)
-
-for first_index, last_index in combinations(range(len(list_numbers)), 2):
-    contiguous_set = list_numbers[first_index:last_index+1]
-    if sum(contiguous_set) == invalid_number:
-        print("solution:", min(contiguous_set) + max(contiguous_set))
-        break
-else:
-    raise Exception("no set found")
+contiguous_set_range = code.find_contiguous_range_set()
+contiguous_set = code[contiguous_set_range.start:contiguous_set_range.stop:contiguous_set_range.step]
+print("solution:", min(contiguous_set) + max(contiguous_set))
