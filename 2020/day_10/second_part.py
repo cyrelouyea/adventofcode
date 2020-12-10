5d746dddcaa2a63ac1680bb13926b5d3b0374f0e
cyrelouyea <alerycserrania@protonmail.com>
2020-12-10 13:10:09 +0100

    Refactor day ten part two

--- 2020/day_10/second_part.py
+++ 2020/day_10/second_part.py
@@ -1,20 +1,23 @@
-from typing import List
+from typing import 
+
+MAX_DIFF = 3
 
 adapters: List[int] = [0]
 adapter = int(input())
 while adapter != -1:
     adapters.append(adapter)
     adapter = int(input())
-adapters.append(max(adapters) + 3)
+adapters.append(max(adapters) + MAX_DIFF)
 adapters.sort()
 
 nb_arrangements = [0] * len(adapters)
 nb_arrangements[-1] = 1
 
 for i in range(len(adapters)-1, -1, -1):
-    for j in (1,2,3,):
-        if i-j >= 0 and adapters[i-j] + 3 >= adapters[i]:
-            nb_arrangements[i-j] += nb_arrangements[i]
+    j = 1
+    while i-j >= 0 and adapters[i-j] + MAX_DIFF >= adapters[i]:
+        nb_arrangements[i-j] += nb_arrangements[i]
+        j += 1
 
 print("solutions:", nb_arrangements[0])
 
