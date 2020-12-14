aef6af84a8b356a2a42936dd62720a966e960cb9
cyrelouyea <alerycserrania@protonmail.com>
2020-12-14 23:56:02 +0100

    Add day two haskell solution

---
+++ 2020/day_2/haskell/first_part.hs
@@ -0,0 +1,74 @@
+module Main where
+
+import System.IO
+
+data Password = Password
+  { min :: Int,
+    max :: Int,
+    letter :: Char,
+    pwd :: [Char]
+  }
+
+getMin (Password min _ _ _) = min
+getMax (Password _ max _ _) = max
+getLetter (Password _ _ letter _) = letter
+getPassword (Password _ _ _ pwd) = pwd
+
+isValid :: Password -> Bool
+isValid (Password min max letter pwd) = do
+  let count = countLetterInStr pwd letter
+  (min <= count) && (count <= max)
+
+doReadFile :: IO [Password]
+doReadFile = do
+  withFile "input" ReadMode doReadLine
+
+doReadLine :: Handle -> IO [Password]
+doReadLine hFile = do
+  isEnd <- hIsEOF hFile
+  if isEnd
+    then return []
+    else do
+      line <- hGetLine hFile
+      rest <- doReadLine hFile
+      return (parseLine line : rest)
+
+parseLine :: [Char] -> Password
+parseLine line = do
+  let (min, r1) = takeMin line
+  let (max, r2) = takeMax r1
+  let (letter, pwd) = takeLetter r2
+  Password min max letter pwd
+
+takeMin :: [Char] -> (Int, [Char])
+takeMin line = do
+  let (min, rest) = splitFirst line '-'
+  (read min, rest)
+
+takeMax :: [Char] -> (Int, [Char])
+takeMax line = do
+  let (max, rest) = splitFirst line ' '
+  (read max, rest)
+
+takeLetter :: [Char] -> (Char, [Char])
+takeLetter line = do
+  let (letter, rest) = splitFirst line ':'
+  (head letter, dropWhile (==' ') rest)
+
+splitFirst :: [Char] -> Char -> ([Char], [Char])
+splitFirst s l 
+  | null s = ("", "")
+  | head s == l = ("", tail s)
+  | otherwise = do
+      let (first, last) = splitFirst (tail s) l
+      (head s : first, last)
+
+countLetterInStr :: [Char] -> Char -> Int
+countLetterInStr s l
+  | null s = 0
+  | head s == l = 1 + countLetterInStr (tail s) l
+  | otherwise = countLetterInStr (tail s) l
+    
+main = do
+  passwords <- doReadFile
+  print(length (filter isValid passwords))
---
+++ 2020/day_2/haskell/second_part.hs
@@ -0,0 +1,80 @@
+module Main where
+
+import System.IO
+
+data Password = Password
+  { first :: Int,
+    second :: Int,
+    letter :: Char,
+    pwd :: [Char]
+  }
+
+getFirst (Password first _ _ _) = first
+getSecond (Password _ second _ _) = second
+getLetter (Password _ _ letter _) = letter
+getPassword (Password _ _ _ pwd) = pwd
+
+isValid :: Password -> Bool
+isValid (Password first second letter pwd) = 
+  isLetterInPos pwd letter (first-1) /= isLetterInPos pwd letter (second-1)
+
+isLetterInPos :: [Char] -> Char -> Int -> Bool
+isLetterInPos pwd letter place
+  | place == -1 = False
+  | null pwd = False
+  | head pwd == letter && place == 0 = True
+  | otherwise = isLetterInPos (tail pwd) letter (place - 1)
+
+doReadFile :: IO [Password]
+doReadFile = do
+  withFile "input" ReadMode doReadLine
+
+doReadLine :: Handle -> IO [Password]
+doReadLine hFile = do
+  isEnd <- hIsEOF hFile
+  if isEnd
+    then return []
+    else do
+      line <- hGetLine hFile
+      rest <- doReadLine hFile
+      return (parseLine line : rest)
+
+parseLine :: [Char] -> Password
+parseLine line = do
+  let (first, r1) = takeFirst line
+  let (second, r2) = takeSecond r1
+  let (letter, pwd) = takeLetter r2
+  Password first second letter pwd
+
+takeFirst :: [Char] -> (Int, [Char])
+takeFirst line = do
+  let (first, rest) = splitFirst line '-'
+  (read first, rest)
+
+takeSecond :: [Char] -> (Int, [Char])
+takeSecond line = do
+  let (second, rest) = splitFirst line ' '
+  (read second, rest)
+
+takeLetter :: [Char] -> (Char, [Char])
+takeLetter line = do
+  let (letter, rest) = splitFirst line ':'
+  (head letter, dropWhile (==' ') rest)
+
+splitFirst :: [Char] -> Char -> ([Char], [Char])
+splitFirst s l 
+  | null s = ("", "")
+  | head s == l = ("", tail s)
+  | otherwise = do
+      let (first, last) = splitFirst (tail s) l
+      (head s : first, last)
+
+countLetterInStr :: [Char] -> Char -> Int
+countLetterInStr s l
+  | null s = 0
+  | head s == l = 1 + countLetterInStr (tail s) l
+  | otherwise = countLetterInStr (tail s) l
+
+main = do
+  passwords <- doReadFile
+  print(length (filter isValid passwords))
