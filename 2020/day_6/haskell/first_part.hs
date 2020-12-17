f6501e47624a0bb79306ff001764a5294a96976c
cyrelouyea <alerycserrania@protonmail.com>
2020-12-17 01:37:12 +0100

    Add day 6 haskell solution

---
+++ 2020/day_6/haskell/first_part.hs
@@ -0,0 +1,36 @@
+module Main where
+
+import Data.List
+import qualified Data.Set as Set
+import System.IO
+
+type Form = Set.Set Char
+
+type Group = [Form]
+
+main :: IO ()
+main = do
+  m <- doReadFile
+  print $ sum $ map (Set.size . Set.unions) (parseGroups m)
+  return ()
+
+parseGroups :: [[Char]] -> [Group]
+parseGroups fs
+  | null fs = []
+  | null rest = [map Set.fromList group]
+  | otherwise = map Set.fromList group : parseGroups (tail rest)
+  where
+    (group, rest) = break null fs
+
+doReadFile :: IO [[Char]]
+doReadFile = withFile "input" ReadMode doReadLine
+
+doReadLine :: Handle -> IO [[Char]]
+doReadLine hFile = do
+  isEnd <- hIsEOF hFile
+  if isEnd
+    then return []
+    else do
+      line <- hGetLine hFile
+      rest <- doReadLine hFile
+      return (line : rest)
